ca035489153f0eac62da27f98396ffc9eec23b0d
DanaiLathong <bankkok159z@gmail.com>
2023-09-22 10:13:06 +0700

    Code V.4 : If - Else - If

---
+++ test04.py
@@ -0,0 +1,11 @@
+#if - else - if
+if 50 < 60 :
+    print ("50 < 60")
+elif "Saranchai" == "Saranchui" :
+    print ("Saranchai" == "Saranchui")
+elif 100 < 80 :
+    print ("100 < 80")
+else :
+    print ("IoT")
+    print ("SAU")
+print ("-----END-----")
