b0824eba6519042183d83e7f4429a3869a307ee3
DanaiLathong <bankkok159z@gmail.com>
2023-09-22 10:10:45 +0700

    Code V.2 : IF 00

---
+++ test2.py
@@ -0,0 +1,7 @@
+num = int(input("Input number : "))
+if num <= 5 :
+    print ("Hi....")
+    print ("Helloo...")
+    print ("Yoo.....")
+    print ("----------------------------------")
+print ("Woo....")
